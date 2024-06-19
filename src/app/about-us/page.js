e7b469704e83504201e8a5481305ab07ceff2628
taherul77 <taherul2317@gmail.com>
2024-06-19 09:01:52 +0600

    create about gallery seminar page

---
+++ src/app/about-us/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      fdgdgsa
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/seminar/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      hhhh
+    </div>
+  )
+}
+
+export default Page
